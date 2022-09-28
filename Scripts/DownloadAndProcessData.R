7cf022cc305d85fde30f20f6d03f44e6a469e244
hannahlowens <hannah.owens@gmail.com>
2022-09-28 13:18:27 +0200

    Minor polishes for readability.

--- Scripts/DownloadAndProcessData.R
+++ Scripts/DownloadAndProcessData.R
@@ -10,16 +10,19 @@ mammothOccurrences <-  pbdb_occurrences (limit="all",
 
 # Filter records for Unique occurrences and relevant columns
 mammothOccurrences <- mammothOccurrences %>% select("taxon_name", "lng", "lat", "early_age", "late_age") %>% unique()
-mammothOccurrencesClean[,c("lng", "lat", "early_age", "late_age")] <- as.numeric(unlist(mammothOccurrencesClean[,c("lng", "lat", "early_age", "late_age")]))
+mammothOccurrencesClean[,c("lng", "lat", "early_age", "late_age")] <- as.numeric(unlist(mammothOccurrencesClean[,c("lng", "lat", 
+                                                                                                                   "early_age", 
+                                                                                                                   "late_age")]))
 
 # Quick plot to see what's there
-plot(mammothOccurrencesClean[,c("lng","lat")], pch = 20, col = "red", main = "All Mammoth Points", xlab = "Longitude", ylab = "Latitude")
+plot(mammothOccurrencesClean[,c("lng","lat")], pch = 20, col = "red", main = "All Mammoth Points", 
+     xlab = "Longitude", ylab = "Latitude")
 plot(gshhs, add = T)
 
 # Split points into relevant time slices ----
 mammothOccurrencesClean$estimated_age <- (mammothOccurrencesClean$early_age + mammothOccurrencesClean$late_age)/2
 
-# Assign time slices to occcurences based on date ranges from paleoclim.org
+# Assign time slices to occurrences based on date ranges from paleoclim.org
 relevantTimeSlices <- data.frame (maxTime = c(3.3, 3.205, .787, 0.130, 0.017, 0.0129, 0.0117, 0.00826, 0.0042), 
                                   minTime = c(3.3, 3.205, .787, 0.130, 0.0147, 0.0117, 0.00826, 0.0042, 0.0003))
 relevantTimeSlices$meanAge <- (relevantTimeSlices$maxTime + relevantTimeSlices$minTime)/2
@@ -36,7 +39,8 @@ mammothOccurrencesClean$Col <- rbPal(ncolors)[mammothOccurrencesClean$timeIndex]
 
 plot(mammothOccurrencesClean[,c("lng","lat")], main = "All Mammoth Points by Date", 
      xlab = "Longitude", ylab = "Latitude", col = mammothOccurrencesClean$Col, pch = 20,)
-legend("topleft",title="Date (My)",legend=relevantTimeSlices$meanAge[sort(unique(mammothOccurrencesClean$timeIndex))],col =rbPal(ncolors),pch=20)
+legend("topleft",title="Date (My)",legend=relevantTimeSlices$meanAge[sort(unique(mammothOccurrencesClean$timeIndex))],
+       col =rbPal(ncolors),pch=20)
 plot(gshhs, add = T)
 
 for (i in 1:length(relevantTimeSlices$meanAge)){
