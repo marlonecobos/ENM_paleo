62778a073912de9761e71dd1977a017a9c6e7aeb
marlonecobos <manubio13@gmail.com>
2022-10-05 16:22:23 -0500

    second big commit, extrapolation risks added here

--- Scripts/model_calibration_projections.R
+++ Scripts/model_calibration_projections.R
@@ -2,7 +2,7 @@
 # Course: ECOLOGICAL MODELS APPLIED TO FOSIL DATA: MODEL CALIBRATION AND
 #         PROJECTIONS
 # Author: Marlon E. Cobos, Hannah L. Owens
-# Date: 04/10/2022
+# Date modified: 05/10/2022
 # ------------------------------------------------------------------------------
 
 
@@ -19,19 +19,16 @@
 
 
 # R packages needed ------------------------------------------------------------
-# these lines load packages (if needed packages are installed first)
-if (!require("remotes")) {
-  install.packages("remotes")
-  Sys.sleep(2)
-  library(remotes)
-}
-if (!require("kuenm")) { # it may be tricky (requires compilation tools)
-  remotes::install_github("marlonecobos/kuenm")
-  Sys.sleep(2)
-  library(kuenm)
-}
+# the next lines of code load packages 
+
+## if needed, install packages (remotes, raster) using 
+## install.packages("package_name")
+## kuenm is on GitHub so install it as 
+## remotes::install_github("marlonecobos/kuenm") 
+## https://github.com/marlonecobos/kuenm#installing-the-package
 
 library(raster)
+library(kuenm)
 # ------------------------------------------------------------------------------
 
 
@@ -55,8 +52,11 @@ pcs <- kuenm_rpca(variables = m_hs1, var.scale = TRUE, project = TRUE,
 
 # preparing data for model calibration
 ## new directory
-dir.create("ENM")
+if (!dir.exists("ENM")) {
+  dir.create("ENM")
+}
 
+## preparing data to run Maxent in SWD format
 data_prep <- prepare_swd(occ = oc_hs1, species = "taxon_name", longitude = "lng",
                          latitude = "lat", data.split.method = "random", 
                          train.proportion = 0.7, 
@@ -71,18 +71,18 @@ data_prep <- prepare_swd(occ = oc_hs1, species = "taxon_name", longitude = "lng"
 
 # Model calibration ------------------------------------------------------------
 # define arguments for simplicity
-oj <- "ENM/hs1_joint.csv"
-otr <- "ENM/hs1_train.csv"
-ote <- "ENM/hs1_test.csv"
-back_dir <- "ENM/background"
-bat <- "ENM/bash"
-dir_candidates <- "ENM/Candidate_models"
-rgmul <- c(0.1, 0.5, 1, 2.5, 5)
-fclas <- c("lq", "lp", "lqp")
-mx <- "/mnt/backup/Maxent"
-sel_type <- "OR_AICc"
-error <- 5
-dir_eval <- "ENM/Calibration_results"
+oj <- "ENM/hs1_joint.csv" # name of file with all records
+otr <- "ENM/hs1_train.csv" # name of file with training records
+ote <- "ENM/hs1_test.csv" # name of file with testing records
+back_dir <- "ENM/background" # name of directory with backgrounds per set of variables 
+bat <- "ENM/bash"  # name of file to write Maxent java code
+dir_candidates <- "ENM/Candidate_models"  # name of directory to write candidate models 
+rgmul <- c(0.1, 0.5, 1, 2.5, 5) # regularization multipliers to test
+fclas <- c("lq", "lp", "lqp") # response types to be used
+mx <- "/mnt/backup/Maxent" # complete path to maxent.jar
+sel_type <- "OR_AICc"  # type of selection to be done
+error <- 5  # omission error allowed (corrected threshold)
+dir_eval <- "ENM/Calibration_results"  # name of directory to write calibration results
 
 # run calibration (create candidate models and selection of best models)
 cal <- kuenm_cal_swd(occ.joint = oj, occ.tra = otr, occ.test = ote, 
@@ -109,7 +109,7 @@ data_prep$sets$Set_4
 # Model projections--------------------------------------------------------------
 # prepare data for projections
 ## directories
-proj_var <- "ENM/Proj_variables"
+proj_var <- "ENM/Proj_variables" # name of directory with projection scenarios
 dir.create(proj_var) # general directory for projection layers 
 
 dir.create("ENM/Proj_variables/Set_4") # directory for Set of variable selected
@@ -131,16 +131,16 @@ for (i in 1:length(pcnames)) {
 
 # project selected model
 ## arguments
-bat_mod <- "ENM/bat_model"
-reps <- 5
-rep_type <- "Bootstrap"
-jack <- TRUE
-out_form <- "cloglog"
-proj <- TRUE
-extrap <- "ext"
-mess <- TRUE
-mod_dir <- "ENM/Final_models"
-wait <- TRUE
+bat_mod <- "ENM/bat_model" # name of file to write Maxent java code 
+reps <- 5 # number of replicates
+rep_type <- "Bootstrap" # type of replicate to be done 
+jack <- TRUE # to get jackknife results 
+out_form <- "cloglog" # Maxent output format
+proj <- TRUE # to project model to other scenarios 
+extrap <- "ext" # type of Maxent extrapolation (free extrapolation)
+mess <- TRUE # to get MESS results
+mod_dir <- "ENM/Final_models" # name of directory to write final model and projections 
+wait <- TRUE # to wait until Maxent finishes  
 
 
 ## final model with projections
@@ -150,3 +150,84 @@ kuenm_mod_swd(occ.joint = oj, back.dir = back_dir, out.eval = dir_eval,
               G.var.dir = proj_var, ext.type = extrap, write.mess = mess, 
               maxent.path = mx, out.dir = mod_dir, wait = wait)
 # ------------------------------------------------------------------------------
+
+
+
+# Extrapolation risk MOP -------------------------------------------------------
+# MOP as in Owens et al. (2013)
+## arguments
+swd <- TRUE
+var_set <- "Set_4"
+mop_dir <- "ENM/MOP_results"
+ref_percent <- 5
+
+## run MOP
+kuenm_mmop(G.var.dir = proj_var, M.var.dir = back_dir, is.swd = swd, 
+           sets.var = var_set, out.mop = mop_dir, percent = ref_percent)
+
+
+# A more detailed version of MOP
+## bring function from GitHub
+source("https://raw.githubusercontent.com/marlonecobos/new_MOP/main/Scripts/MOP_function.R")
+
+## calibration background
+back <- read.csv("ENM/background/Set_4.csv")
+back <- as.matrix(back[, 4:6])
+
+## variable stacks for projections were prepared in the first section
+
+## other arguments
+type_mop <- "detailed"
+dist_type <- "euclidean"
+scal <- TRUE
+cent <- TRUE
+rescal <- FALSE
+
+## run MOP for projection to hs1
+mop_hs1 <- mop(m = back, g = pcs$PCRasters_initial, mop_type = type_mop, 
+               distance = dist_type, scale = scal, center = cent, 
+               percent = ref_percent, rescale_mop = rescal)
+
+## run MOP for projection to msi19
+mop_msi19 <- mop(m = back, g = pcs$PCRasters_Projected_PCs, mop_type = type_mop, 
+                 distance = dist_type, scale = scal, center = cent, 
+                 percent = ref_percent, rescale_mop = rescal)
+# ------------------------------------------------------------------------------
+
+
+
+# Plots of extrapolation risk analyses -----------------------------------------
+# MESS results
+novel_hs1 <- raster("ENM/Final_models/M_5_F_lqp_Set_4_E/Mammut_americanum_0_hs1_novel.asc")
+novel_msi19 <- raster("ENM/Final_models/M_5_F_lqp_Set_4_E/Mammut_americanum_0_msi19_novel.asc") 
+
+nov_lim_hs1 <- raster("ENM/Final_models/M_5_F_lqp_Set_4_E/Mammut_americanum_0_hs1_novel_limiting.asc")
+nov_lim_msi19 <- raster("ENM/Final_models/M_5_F_lqp_Set_4_E/Mammut_americanum_0_msi19_novel_limiting.asc") 
+
+plot(novel_hs1)
+plot(novel_msi19)
+
+plot(nov_lim_hs1, col = c("orange", "red", "white"))
+plot(nov_lim_msi19, col = c("orange", "red", "white"))
+
+
+# results from kuenm MOP
+kuenm_mop_hs1 <- raster("ENM/MOP_results/Set_4/MOP_5%_hs1.tif")
+kuenm_mop_msi19 <- raster("ENM/MOP_results/Set_4/MOP_5%_msi19.tif") 
+
+plot(kuenm_mop_hs1)
+plot(kuenm_mop_msi19)
+
+# more detailed MOP results
+plot(mop_hs1$mop_basic)
+plot(mop_hs1$mop_simple, col = c("orange", "red"))
+plot(mop_hs1$mop_detailed$towards_low_end, col = "blue")
+plot(mop_hs1$mop_detailed$towards_high_end, col = "red")
+
+
+plot(mop_msi19$mop_basic)
+plot(mop_msi19$mop_simple, col = c("orange", "red"))
+plot(mop_msi19$mop_detailed$towards_low_end, col = "blue")
+plot(mop_msi19$mop_detailed$towards_high_end, col = "red")
+# ------------------------------------------------------------------------------
+
